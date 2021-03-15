29847665d78cf337c56c0eeeef7e365823556c9e
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2021-03-15 18:16:01 +0300

    update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,53 @@ const restaurant = {
       close: 24,
     },
   },
-};
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
+  orderDelivery: function({starterIndex= 1, mainIndex=0, time='20:00', address}) {
+    console.log(`Order received: ${this.starterMenu[starterIndex]} and 
+    ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  }
+}
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'via del sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+})
+
+restaurant.orderDelivery({
+  address: 'via del sole, 21',
+  starterIndex: 1,
+})
+
+
+
+/*const { name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const { name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = []} = restaurant;
+
+console.log(menu, starters);
+
+// mutating variables
+
+let a = 111;
+let b = 999;
+
+const obj = {a: 23, b: 7, c: 14};
+
+({a,b} = obj)
+console.log(a,b);
+
+// nested objects
+
+const { fri: {open: o, close: c} } = openingHours;
+
+console.log(o, c);*/
